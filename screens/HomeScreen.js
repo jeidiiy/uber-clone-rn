46de1d292c569d5c2f8e1ab1555cb1979aab05a9
Ji-Yong, Jeong <jeidiiy98@gmail.com>
2022-07-31 14:32:12 +0900

    feat: Google Places Autocomplete 적용

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,8 +3,14 @@ import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 import GlobalStyles from '../GlobalStyles';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_API_KEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slice/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={(tw`bg-white h-full`, GlobalStyles.AndroidSafeArea)}>
       <View style={tw`p-5`}>
@@ -13,6 +19,36 @@ const HomeScreen = () => {
           source={{
             uri: 'https://links.papareact.com/gzs',
           }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: 'ko',
+          }}
+          fetchDetails={true}
+          returnKeyType={'search'}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              }),
+            );
+
+            dispatch(setDestination(null));
+          }}
+          enablePoweredByContainer={false}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
         />
         <NavOptions />
       </View>
